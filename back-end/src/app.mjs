1bb7818442845712a8b531e269ebdfe22e1c74da
ebc5802 <edison.chen@stern.nyu.edu>
2024-12-11 17:56:17 -0500

    add cors middleware

--- back-end/src/app.mjs
+++ back-end/src/app.mjs
@@ -53,6 +53,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
+// CORS middleware
+const corsMiddleware = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  ); // Allowed HTTP methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  next();
+};
+app.use(corsMiddleware);
+
 // Routes for auth
 app.use("/api/auth", authRoutes);
 
